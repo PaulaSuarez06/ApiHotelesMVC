0114bbc4e7d42dcb0d6808e0fe82b147ed18d1b0
Paula Suarez <paulaelqh@gmail.com>
2025-11-22 11:00:09 +0100

    first commit

---
+++ src/test/java/es/daw/apihotelesmvc/ApiHotelesMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package es.daw.apihotelesmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiHotelesMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
